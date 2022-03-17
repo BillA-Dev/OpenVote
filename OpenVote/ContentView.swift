c0b7c8001a388ee82cf4a8cb8dd50512a9eb5f4d
BillA-Dev <90310148@ep-student.org>
2022-03-17 14:44:54 -0500

    Initial Commit

---
+++ OpenVote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OpenVote
+//
+//  Created by 90310148 on 3/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OpenVote/OpenVoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenVoteApp.swift
+//  OpenVote
+//
+//  Created by 90310148 on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct OpenVoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
